63fe103f425d4f2ae725c11142dd1a80f002fc1f
ChandiniGujju <chandinigujju4@gmail.com>
2023-10-12 04:44:05 +0530

    Add files via upload

--- doubly_linkedlist_insertion.cpp
+++ doubly_linkedlist_insertion.cpp
@@ -11,6 +11,15 @@ public:
         next=NULL;
     }
 };
+
+
+
+
+
+
+
+
+
 void insert_at_head(node* &head,int val){
     node* n=new node(val);
     n->next=head;
@@ -18,7 +27,14 @@ void insert_at_head(node* &head,int val){
         head->prev=n;
     head=n;
 }
-void insert_at_tail(node* &head,int val){
+
+
+
+
+
+
+
+void insert_at_tail(node* &head,int val){//time complexity O(n)
     if(head==NULL){
         insert_at_head(head,val);
         return;
@@ -31,20 +47,60 @@ void insert_at_tail(node* &head,int val){
     temp->next=n;
     n->prev=temp;
 }
-void insert_at_pos(node* &head,int val,int pos){
+
+
+
+
+
+
+
+void insert_at_tail1(node* &head,node* &tail,int val){//time complexity O(1)
     node* n=new node(val);
+    if(tail==NULL){
+        head=n;
+        tail=n;
+        return;
+    }
+    tail->next=n;
+    n->prev=tail;
+    tail=n;
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+void insert_at_position(node* head,int val,int p){
+    node* n=new node(val);
+    n->data=val;
     if(head==NULL){
         insert_at_head(head,val);
         return;
     }
-    n->data=val;
     node* temp=head;
-    for(int i=0;i<pos-1;i++){
+    for(int i=0;i<p-1;i++){
         temp=temp->next;
     }
     n->next=temp->next;
     temp->next=n;
+    n->prev=temp;
+    n->next->prev=n;
+
 }
+
+
+
+
+
+
+
 void print(node* head){
     node* temp=head;
     while(temp!=NULL){
@@ -53,17 +109,24 @@ void print(node* head){
     }
     cout<<endl;
 }
+
+
+
+
+
+
 int main(){
     node* head=NULL;
-    insert_at_tail(head,10);
-    insert_at_tail(head,12);
+    node* tail=NULL;
+    insert_at_tail1(head,tail,10);
+    insert_at_tail1(head,tail,12);
     print(head);
     cout<<endl;
     insert_at_head(head,8);
     insert_at_head(head,6);
     print(head);
-    insert_at_pos(head,4,2);
-    insert_at_pos(head,9,4);
+    insert_at_position(head,4,2);
+    insert_at_position(head,9,4);
     print(head);
     cout<<endl;
 }
