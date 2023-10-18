9549842a498d0618ae2deb0956cf23a3aadfcb47
ChandiniGujju <chandinigujju4@gmail.com>
2023-10-18 23:21:34 +0530

    Add files via upload

---
+++ trees_creation_traversals.cpp.cpp
@@ -0,0 +1,91 @@
+
+//creation of tree
+
+#include<iostream>
+using namespace std;
+class node{
+public:
+    int data;
+    node* left;
+    node* right;
+    node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+node* buildTree(node* &root){
+    cout<<"enter the data: "<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    if(data==-1){
+        return NULL;
+    }
+    cout<<"enter the left element of "<<data<<endl;
+    root->left=buildTree(root->left);
+    cout<<"enter the right element : "<<data<<endl;
+    root->right=buildTree(root->right);
+    return root;
+}
+
+
+
+
+//traversals
+//preorder
+void preorder(node* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+
+
+
+//inorder
+void inorder(node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+
+
+
+//postorder
+void postorder(node* root){
+    if(root==NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+
+
+
+
+int main(){
+    node* root=NULL;
+    root=buildTree(root);
+    preorder(root);
+    cout<<endl;
+    inorder(root);
+    cout<<endl;
+    postorder(root);
+}
+
+
+
+
+
+
+
